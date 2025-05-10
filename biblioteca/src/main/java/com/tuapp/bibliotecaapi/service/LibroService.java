214e3837513a6904afc92ed51ee02b274f1c6de0
Daniel Ramos Montoya <167306596+pidaisaca@users.noreply.github.com>
2025-05-10 18:15:01 +0100

    Update LibroService.java

--- biblioteca/src/main/java/com/tuapp/bibliotecaapi/service/LibroService.java
+++ biblioteca/src/main/java/com/tuapp/bibliotecaapi/service/LibroService.java
@@ -1,5 +1,45 @@
 package com.tuapp.bibliotecaapi.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tuapp.bibliotecaapi.model.Libro;
+import com.tuapp.bibliotecaapi.repository.LibroRepository;
+
+@Service
 public class LibroService {
+
+    private final LibroRepository libroRepository;
+
+    public LibroService(LibroRepository libroRepository) {
+        this.libroRepository = libroRepository;
+    }
+
+    public List<Libro> getAllLibros() {
+        return libroRepository.findAll();
+    }
+    
+    public Libro getLibroById(Long id) {
+        return libroRepository.findById(id).orElse(null);
+    }
+
+    public Libro crearLibro(Libro libro) {
+        return libroRepository.save(libro);
+    }
+
+    public Libro updateLibro(Libro libro) {
+        return libroRepository.save(libro);
+    }
+
+    public void deleteLibro(Long id) {
+        libroRepository.deleteById(id);
+    }
+
+    public List<Libro> buscar() {
+        List<Libro> libros = libroRepository.findAll();
+        // falta aqui
+        return libros;
+    }
 
 }
