b021b9906ab95b0baddb78da2c77f8284654b7fa
Daniel Ramos Montoya <167306596+pidaisaca@users.noreply.github.com>
2025-05-10 18:08:15 +0100

    Actualización 1
    Clases relacionadas con Libro y sus métodos

---
+++ bibliotecaapi/model/Libro.java
@@ -0,0 +1,81 @@
+package com.tuapp.bibliotecaapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Libro")
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_libro;
+
+    private String titulo;
+    private String ISBN;
+    private int anioPublicacion;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+
+    public Libro() {
+    }
+
+    public Libro(Long id_libro, String titulo, String ISBN, int anioPublicacion, Autor autor) {
+        this.id_libro = id_libro;
+        this.titulo = titulo;
+        this.ISBN = ISBN;
+        this.anioPublicacion = anioPublicacion;
+        this.autor = autor;
+    }
+
+    public Long getId_libro() {
+        return id_libro;
+    }
+
+    public void setId_libro(Long id_libro) {
+        this.id_libro = id_libro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String iSBN) {
+        ISBN = iSBN;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    
+
+
+}
